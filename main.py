e6b2c1e4fef5af5f91b96a79c27de9ae01ba3386
Luis <pereiravazluis@gmail.com>
2021-07-14 04:41:02 +0200

    :lipstick: Update UI display rdvCovid

--- main.py
+++ main.py
@@ -63,18 +63,17 @@ def good_channel(ctx):
 
 async def covid_traitment(ctx, app_count, nom_labo, address_labo, next_rdv, url, vt):
     if app_count > 0:
-
-        embed = discord.Embed(title="Doses disponibles...", description="", url="https://github.com/Lutenruto/", color=0x24D2B5)
+        embed = discord.Embed(title="Doses disponibles...", description="", url="https://github.com/Lutenruto/", color=0xc03537)
         embed.set_author(name=ctx.author.name,
                          icon_url=ctx.author.avatar_url,
                          url="https://github.com/Lutenruto/")
         embed.set_thumbnail(url="https://nsa40.casimages.com/img/2021/07/14/210714035657987298.png")
-        embed.add_field(name="Voici le résultat des recherches",
-                        value=f"A {nom_labo}, à l'adresse : https://www.google.com/maps/search/{address_labo.replace(' ', '+')}\n"
-                              f"il y a {app_count} créneaux disponibles.\n"
-                              f"Le prochain rdv est disponible le : {next_rdv}\n"
-                              f"Voici l'url : {url}\n"
-                              f"Type de vaccin : {vt}\n"
+        embed.add_field(name=f"{nom_labo}",
+                        value=f"Adresse : https://www.google.com/maps/search/{address_labo.replace(' ', '+')}\n"
+                              f"Il y a **{app_count}** créneaux disponibles.\n"
+                              f"Le prochain rdv est disponible le : **{next_rdv}**\n"
+                              f"URL : {url}\n"
+                              f"Type de vaccin : **{vt}**\n"
                         , inline=False)
 
         embed.set_footer(text="Pour plus d'information, contacter Lutenruto")
@@ -87,7 +86,7 @@ async def covid_traitment(ctx, app_count, nom_labo, address_labo, next_rdv, url,
 async def rdv_covid(ctx, department, *, vac_type=""):
     cp = ""
     vac_present = False
-    vaccines = ["AstraZeneca", "Janssen", "Pfizer-BioNTech", "Moderna", "CureVac"]
+    vaccines = ["AstraZeneca", "Janssen", "Pfizer-BioNTech", "Moderna", "CureVac", "ARNm"]
     if len(vac_type) > 0:
         for vac in vaccines:
             if vac_type in vac:
@@ -107,7 +106,10 @@ async def rdv_covid(ctx, department, *, vac_type=""):
         app_count = centre.get("appointment_count", [])
         nom_labo = centre.get("nom", [])
         address_labo = centre.get("metadata", []).get("address", [])
-        code_postal = centre.get("location", []).get("cp", [])
+        try:
+            code_postal = centre.get("location", []).get("cp", [])
+        except:
+            code_postal = cp
         vaccine_type = centre.get("vaccine_type", [])
 
         prochain_rdv = centre.get("prochain_rdv", [])
@@ -309,7 +311,7 @@ async def command_display(ctx):
                           "```example : "
                           "!rdvCovid 75 Pfizer\n"
                           "!rdvCovid 75005 Astra\n"
-                          "Type of vaccin : AstraZeneca, Janssen,\nPfizer-BioNTech, Moderna, CureVac"
+                          "Type of vaccin : AstraZeneca, Janssen,\nPfizer-BioNTech, Moderna, CureVac, ARNm"
                           "```"
                           "**!getPrice** => Retrieve asset information from Binance\n"
                           "```example : !getPrice btc/usdt```"
